daa147351dd4860d320d2b1a87af598dee99e041
KIENPN <kienpndev@gmail.com>
2023-04-14 16:12:56 +0700

    load code

--- view/nuoc.php
+++ view/nuoc.php
@@ -1,7 +1,7 @@
 <?php
 include "header.php";
 ?>
-<link rel="stylesheet" href="./assets/css/nuoc1.css">
+<link rel="stylesheet" href="./assets/css/nuoc.css">
 
 
 <!-- tạo giao diện sắp xếp -->
@@ -14,64 +14,18 @@ include "header.php";
 </div>
 
 <?php
-include "nuoc_hien_thi.php"
+include "../controller/controller_nuoc_va_tp/nuoc_hien_thi.php"
 ?>
 
 <div class='clear'></div>
 
 <!-- tạo chỉ mục trang -->
 <!-- them--popup -->
-<div class='nuoc__modal--popup'>
-    <div class='nuoc__modal__div--popup'>
-        <i><b><u class='nuoc__modal__div--u'>Thêm sản phẩm</u></b></i>
-    <div >
-    <form action="">
-        <table class='nuoc__table--addform'>
-            <tr>
-                <td><label for="fname">Tên:</label></td>
-                <td><input class='nuoc__table--add_input' type="text" id="nuoc__table--add_ten" name="nuoc__table--add_ten"></td>
-            </tr>
-            <tr>
-                <td><label for="lname">Tuổi:</label></td>
-                <td><input class='nuoc__table--add_input' type="text" id="nuoc__table--add_tuoi" name="nuoc__table--tuoi"></td>
-            </tr>
-            <tr>
-                <td><label for="lname">Giới tính:</label></td>
-                <td><select class='nuoc__table--add_input nuoc__table--option'>
-                <option value="nam">Nam</option>
-                <option value="nu">Nữ</option>
-                <option value="khac">Khác</option>
-                </select>
-                </td>
-            </tr>
-            <tr>
-                <td><label for="lname">Ngày sinh:</label></td>
-                <td><input class='nuoc__table--add_input' type="text" id="nuoc__table--add_ngay_sinh" name="nuoc__table--add_ngay_sinh"></td>
-            </tr>
-            <tr>
-                <td><label for="lname">CMND:</label></td>
-                <td><input class='nuoc__table--add_input' type="text" id="nuoc__table--add_cmnd" name="nuoc__table--add_cmnd"></td>
-            </tr>
-            <tr>
-                <td><label for="lname">SĐT:</label></td>
-                <td><input class='nuoc__table--add_input' type="text" id="nuoc__table--add_sdt" name="nuoc__table--add_sdt"></td>
-            </tr>
-            <tr>
-                <td><label for="lname">Biển số xe:</label></td>
-                <td><input class='nuoc__table--add_input' type="text" id="nuoc__table--add_bien_so_xe" name="nuoc__table--add_bien_so_xe"></td>
-            </tr>
-            <tr>
-                <td colspan='2'>
-                    <button class='nuoc__table--add-button nuoc__table--button_huy' type="button" onclick="">Hủy</button>
-                    <input class='nuoc__table--add-button' type="Submit" value="Thêm">
-                </td>
-            </tr>
-        </table>
-    </form>
-    </div>
-    </div>
-</div>
-<div class="clear"></div>
+
+<?php include "view_nuoc_popup.php"
+?>
+<!-- Thêm nước -->
+
 <!-- tạo giao diện nút thêm, sửa, xóa -->
 <div class='nuoc__div--chua_button'>
     <button class='nuoc__div--button js-them' type="button" onclick="">Thêm</button>
@@ -85,6 +39,7 @@ include "nuoc_hien_thi.php"
     const nuocThem = document.querySelectorAll('.js-them')
     const nuocModal = document.querySelector('.nuoc__modal--popup')
     const nuocClose = document.querySelectorAll('.nuoc__table--button_huy')
+    const nuocThemPupUp = document.querySelectorAll('.nuoc__table--button_them')
     function showAdd() {
         nuocModal.classList.add('nuoc__modal--popup--open')
     }
@@ -95,7 +50,76 @@ include "nuoc_hien_thi.php"
         nuocModal.classList.remove('nuoc__modal--popup--open')
     }
     nuocClose[0].addEventListener('click',close)
+
+    function themPupUp() {
+        nuocModal.classList.remove('nuoc__modal--popup--open')
+    }
+
+    nuocThemPupUp[0].addEventListener('click',themPupUp)
+ 
 </script>
+
+<script>
+  // Lấy tất cả các hàng trong bảng
+  var rows = document.getElementsByTagName("tr");
+
+  // Biến lưu trữ trạng thái hiện tại của bảng
+  var currentRow = null;
+
+  // Đặt sự kiện click cho mỗi hàng
+  for (var i = 0; i < rows.length; i++) {
+    rows[i].onclick = function() {
+      // Kiểm tra xem một hàng khác đã được chọn trước đó hay chưa
+      if (currentRow != null) {
+        // Nếu có, đặt lại màu nền của hàng đó
+        currentRow.style.backgroundColor = "";
+      }
+      // Lấy giá trị của các ô trong hàng được kích hoạt
+      var cells = this.getElementsByTagName("td");
+      var cellID = cells[0].innerHTML;
+      currentRow = this;
+      this.style.backgroundColor = "yellow";
+
+    //   // Tạo đối tượng XMLHttpRequest
+    //   var xhttp = new XMLHttpRequest();
+
+    //   // Định nghĩa callback function để xử lý kết quả trả về từ máy chủ
+    //   xhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //       console.log("Dữ liệu đã được gửi thành công!");
+    //     }
+    //   };
+
+    //   // Chỉ định URL và phương thức HTTP
+    //   var url = "../controller/controller_nuoc_va_tp/nuoc_xoa.php";
+    //   var method = "POST";
+
+    //   // Chỉ định các tham số để gửi dữ liệu
+    //   var params = "nuocID=" + cellID;
+    //   console.log(params);
+
+    //   // Mở kết nối đến máy chủ và gửi yêu cầu HTTP POST
+    //   xhttp.open(method, url, true);
+    //   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    //   xhttp.send(params);
+    }
+  }
+
+//     const nuocXoa = document.querySelectorAll('.js-xoa')
+//     function xoaNuoc() {
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       // Xử lý kết quả trả về từ file PHP
+//       console.log(this.responseText);
+//     }
+//   };
+//     xhttp.open("POST", "../controller/controller_nuoc_va_tp/nuoc_xoa.php", true);
+//     xhttp.send();
+// }
+//     nuocXoa[0].addEventListener('click',xoaNuoc)
+</script>
+
 
 </body>
 </html>
