3c20b4eddc149cebcdf999f1aacf7a2d47452ecc
Tung <lmt3997@gmail.com>
2023-04-21 21:04:23 +0700

    lan1

--- view/header.php
+++ view/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style1.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="./assets/fonts/fontawesome-free-6.4.0-web/css/all.min.css">
     <title>GYM HUCE N6 PM1</title>
 </head>
