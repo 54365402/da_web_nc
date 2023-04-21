3b2fd59bde056b028c3ba528bf72cf55f99cd41e
KIENPN <kienpndev@gmail.com>
2023-04-21 21:08:10 +0700

    tach js khoi nuoc.php

--- controller/controller_nuoc_va_tp/nuoc_xoa.php
+++ controller/controller_nuoc_va_tp/nuoc_xoa.php
@@ -1,11 +1,12 @@
 <!-- Kết nối CSDL -->
 <?php
-    include "../connection.php";
+    // include "../connection.php";
     $nuocID = $_POST['nuocID'];
-    $sql = "DELETE FROM tbl_nuoc_va_thuc_pham WHERE id_nuoc_va_tp='$nuocID'";
-    $query = mysqli_query($mysqli,$sql);
-    $mysqli->close();
-    header("Location: ../../view/nuoc.php");
-    exit();
+    echo $nuocID;
+    // $sql = "DELETE FROM tbl_nuoc_va_thuc_pham WHERE id_nuoc_va_tp='$nuocID'";
+    // $query = mysqli_query($mysqli,$sql);
+    // $mysqli->close();
+    // header("Location: ../../view/nuoc.php");
+    // exit();
 
 ?>
