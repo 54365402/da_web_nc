676787893702a17b5ebc0d21a4104d01bed09bf6
KIENPN <kienpndev@gmail.com>
2023-05-13 09:14:08 +0700

    fix

--- view/dang_ky.php
+++ view/dang_ky.php
@@ -84,16 +84,9 @@ I agree to the <a href="#">terms</a>
         echo "<span style='color:red;'>".$_SESSION["nhap_all"]."</span>";
     }
 ?>
-<div class="social-auth-links text-center">
+<div class="social-auth-links text-center"> 
 <p>- OR -</p>
-<a href="#" class="btn btn-block btn-primary">
-<i class="fab fa-facebook mr-2"></i>
-Sign up using Facebook
-</a>
-<a href="#" class="btn btn-block btn-danger">
-<i class="fab fa-google-plus mr-2"></i>
-Sign up using Google+
-</a>
+
 </div>
 <a href="dang_nhap.php" class="text-center">I already have a membership</a>
 </div>
--- view/dang_nhap.php
+++ view/dang_nhap.php
@@ -78,12 +78,7 @@ Remember Me
 
 <div class="social-auth-links text-center mb-3">
 <p>- OR -</p>
-<a href="#" class="btn btn-block btn-primary">
-<i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-</a>
-<a href="#" class="btn btn-block btn-danger">
-<i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-</a>
+
 </div>
 
 <p class="mb-1">
