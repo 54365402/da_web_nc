81ae134918fb7c76de16490eb2c9336b5aea4775
KIENPN <kienpndev@gmail.com>
2023-05-20 17:41:25 +0700

    back data

--- controller/controller_ktc/dang_nhap.php
+++ controller/controller_ktc/dang_nhap.php
@@ -5,7 +5,6 @@
     $email = $_POST["email"];
     $password = $_POST["password"];
     $remeber = $_POST["remember"];
-   // $remeber = $_POST["remember"];
 
 
     $sql = "SELECT * FROM account WHERE username='$email' and password='$password'";
@@ -15,7 +14,6 @@
         $_SESSION['user'] = $email;
         $_SESSION['pass'] = $password;
         $_SESSION['error_login'] = "";
-       // $_SESSION['login'] = true;
     }
     else{
         $_SESSION['login'] = false;
