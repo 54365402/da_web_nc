f07d0f50d0e7c7cd316ff4ce9c92e7873bfef716
KIENPN <kienpndev@gmail.com>
2023-05-23 20:43:52 +0700

    fix bang luong

--- controller/controller_nhan_vien/bang_luong/bl_delete.php
+++ controller/controller_nhan_vien/bang_luong/bl_delete.php
@@ -1,6 +1,6 @@
 <!-- Kết nối CSDL -->
 <?php
-    include_once "../connection.php";
+    include_once "../../connection.php";
     $blID = $_POST['blID'];
     $sql = "DELETE FROM luong WHERE id_nv ='$blID'";
     $query = mysqli_query($mysqli,$sql);
--- controller/controller_nhan_vien/bang_luong/bl_update.php
+++ controller/controller_nhan_vien/bang_luong/bl_update.php
@@ -1,12 +1,17 @@
 <!-- Kết nối CSDL -->
 <?php
-    include_once "../connection.php";
-    $idnv = $_POST['blidnv'];
+    include_once "../../connection.php";
+    $idnv = $_POST['blID'];
     $time_start = $_POST['bltime_start'];
     $so_cong = $_POST['blso_cong'];
     $luong_tren_cong = $_POST['blluong_tren_cong'];
     $luong = $_POST['blluong'];
     $trang_thai = $_POST['bltrang_thai'];
+    if( $trang_thai=="Đã thanh toán")
+            {
+                $trang_thai=1;
+            }
+    else{$trang_thai=0;}
 
     $sql = "UPDATE luong SET time_start='".$time_start."',so_cong=".$so_cong.
     ", luong_tren_cong='".$luong_tren_cong."', luong='".$luong."', trang_thai=".$trang_thai.
